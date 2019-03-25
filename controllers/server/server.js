1bb560c704b3db9e2baf4c9c83a9d8594fac5cb5
xiangqingyi <1352493573@qq.com>
2019-03-25 11:46:59 +0800

    「ADD」 增加后台对dishes操作的接口

--- controllers/server/server.js
+++ controllers/server/server.js
@@ -69,3 +69,175 @@ exports.indexHandler = async (req, res) => {
         return res.redirect('/login')
     }
 }
+
+// 后台首页
+exports.adminIndex = async (req, res) => {
+    const restaurantId = req.params.restaurantId;
+    const sql = "SELECT * FROM t_dishes WHERE restaurant_id=?";
+    //查询菜单表
+    mysqlClientInstance.exec(sql, [restaurantId], function(err, rows) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (!rows) {
+                rows = [];
+            }
+            return res.render('admin/dishes/index', {
+                restaurantId: restaurantId
+            })
+        }
+    })
+}
+
+
+// 后天加菜品
+exports.addDish = async (req, res) => {
+    if (req.method === 'GET') {
+        return res.render('admin/dishes/add', {
+            restaurantId: req.params.restaurantId
+        })
+    } else if (req.method === 'POST') {
+        const dishes = req.body;
+
+        if (dishes) {
+            const sql = "INSERT INTO t_dishes (icon, name, price, level, levelName, classify, restaurant_id) VALUES (?, ?, ?, ?, ?, ?, ?)";
+            const icon = dishes.icon;
+            const name = dishes.name;
+            const price = dishes.price;
+            const level = 0;
+            const levelName = "超级好吃";
+            const classify = dishes.classify;
+            const restaurant_id = req.params.restaurantId;
+            mysqlClientInstance.exec(sql, [icon, name, price, level, levelName, classify, restaurant_id], function(err, rows) {
+                if (err) {
+                    return res.send({
+                        code: 1,
+                        message: '失败',
+                        status: false
+                    })
+                } else {
+                    return res.send({
+                        code: 0,
+                        message: '操作成功',
+                        status: true
+                    })
+                }
+            })
+        } else {
+            return res.send({
+                code: 1,
+                message: '失败',
+                status: false
+            })
+        }
+    }
+}
+
+// 删除某个餐馆所有的菜
+exports.deleteAll = async (req, res) => {
+    const restaurantId = req.params.restaurantId;
+    const sql = "DELETE FROM t_dishes WHERE restauran_id=?";
+    mysqlClientInstance.exec(sql, [restaurantId],function(err, rows) {
+        if (err) {
+            console.log(err);
+            return res.send({
+                code: 1,
+                message: '删除失败'
+            })
+        } else {
+            return res.send({
+                code: 0,
+                message: '删除成功'
+            })
+        }
+    })
+}
+
+// 删除某个餐馆指定的某个菜
+exports.deleteDish = async (req, res) => {
+    const sql = "DELETE FROM t_dishes WHERE id=?";
+    const id = req.params.id;
+    mysqlClientInstance.exec(sql, [id], function(err, rows) {
+        if (err) {
+            return res.send({
+                code: 1,
+                status: false,
+                message: '删除失败'
+            })
+        } else {
+            return res.send({
+                code: 0,
+                message: '删除成功',
+                status: true
+            })
+        }
+    })
+}
+
+// 显示指定id的菜品
+exports.showDish = async (req, res) => {
+    const sql = "SELECT * FROM t_dishes WHERE id = ?";
+    const id = req.params.id;
+    mysqlClientInstance.exec(sql, [id], function(err, rows) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (!rows || rows.length === 0) {
+                return res.send({
+                    code: 1,
+                    message: '没有找到指定id的餐厅'
+                })
+            } else {
+                return res.render('admin/dishes/detail', {
+                    dishes: rows[0]
+                })
+            }
+        }
+    })
+}
+
+// 修改餐厅的信息
+exports.editRestaurant = async (req, res) => {
+    if (req.method === 'GET') {
+        const sql = "SELECT * FROM t_dishes WHERE id=?";
+        const id = req.params.id;
+        mysqlClientInstance.exec(sql, [id], function(err, rows) {
+            if (err) {
+                console.log(err);
+                return res.send({
+                    code: 1,
+                    message: '获取餐厅信息失败'
+                })
+            } else {
+                return res.render('admin/dishes/edit',{
+                    restaurantId: id
+                })
+            }
+        })
+    } else if (req.method === 'POST') {
+        const id = req.params.id;
+        const dishes = req.body;
+        if (id && dishes) {
+            const sql = "UPDATE t_dishes SET icon=?, name=?,level=?,levelName=?, classify=? WHERE id="+id;
+            const icon = dishes.icon;
+            const name = dishes.name;
+            const price = dishes.price;
+            const level = 0;
+            const levelName = "超级好吃";
+            const classify = dishes.classify;
+            mysqlClientInstance.exec(sql, [icon, name, price, level, levelName, classify], function (err, result) {
+                if (err) {
+                    return res.send({
+                        code: 1,
+                        message: "更新餐厅信息失败"
+                    })
+                } else {
+                    return res.send({
+                        code: 0,
+                        message: "更新餐厅信息失败"
+                    })
+                }
+            })
+        }
+    }
+}
